82d620ce6024dfd7d03e4684fc62aca1b1238345
Oleh Khrystenko <50414500+Kh-Ol-An@users.noreply.github.com>
2025-11-28 14:05:37 +0200

    Fix Sanity studio structure crash

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,9 +1,26 @@
 import type {StructureResolver} from 'sanity/structure'
 
+const singletonTypes = new Set(['pageSettings'])
+
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-export const structure: StructureResolver = (S) =>
-  S.list()
+export const structure: StructureResolver = (S) => {
+  const documentTypeItems = S.documentTypeListItems().filter(
+    (listItem) => listItem.getId() && !singletonTypes.has(listItem.getId() ?? ''),
+  )
+
+  return S.list()
     .title('Content')
     .items([
-      S.documentTypeListItem('pageSettings').title('Налаштування сторінки'),
+      S.listItem()
+        .title('Налаштування сторінки')
+        .id('pageSettings')
+        .child(
+          S.document()
+            .schemaType('pageSettings')
+            .documentId('pageSettings')
+            .title('Налаштування сторінки'),
+        ),
+      S.divider(),
+      ...documentTypeItems,
     ])
+}
